ac9b6d00566ea3e94b3203e7ceba716c2a0a4db9
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2026-05-18 20:46:58 -0300

    commit 3

--- aula4.py
+++ aula4.py
@@ -3,3 +3,17 @@ print("Hello, World!")
 print(11)
 print(-11)
 print(0)
+
+print(type(1))
+print(type(-11))
+print(type(0))
+
+print(1.5)
+print(-1.5)
+print(type(1.5))
+print(type(-1.5))
+
+print(10 == 10)
+print(10 == 11)
+print(type(10 == 10))
+print(type(10 == 11))
